027ec6f93bd12048f7fac93d1e7f56df861ca55e
rathod.gauravvinod@gmail.com <rathod.gauravvinod@gmail.com>
2022-06-11 15:30:09 +0530

    day3: view blogs, update a blog, delete a blog, and logout functionality added

--- Blog/home/urls.py
+++ Blog/home/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     path('login/', login_view, name='login_view'),
     path('register/', register_view, name='register_view'),
     path('add-blog/', add_blog, name='add_blog'),
-    path('blog_detail/<slug>', blog_detail, name='blog_detail'),
+    path('blog-detail/<slug>', blog_detail, name='blog_detail'),
+    path('see-blog/', see_blog, name='see_blog'),
+    path('blog-delete/<id>', blog_delete, name="blog_delete"),
+    path('blog-update/<slug>', blog_update, name="blog_update"),
+    path('logout-view', logout_view, name='logout_view'),
+
 ]
--- Blog/home/views.py
+++ Blog/home/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, redirect
 from .forms import *
 
+from django.contrib.auth import logout
+
 # Create your views here.
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
 
 def home(request):
     context = {'blogs': BlogModel.objects.all()}
@@ -40,6 +46,53 @@ def blog_detail(request, slug):
     except Exception as e:
         print(e)
     return render(request, 'blog_detail.html', context)
+
+def see_blog(request):
+    context = {}
+    try:
+        blog_objs = BlogModel.objects.filter(user = request.user)
+        context['blog_objs'] = blog_objs
+    except Exception as e:
+        print(e)
+    return render(request, 'see_blog.html', context)
+
+def blog_delete(request, id):
+    try:
+        blog_obj = BlogModel.objects.get(id = id)
+        if blog_obj.user == request.user:
+            blog_obj.delete()
+    except Exception as e:
+        print(e)
+    return redirect('/see-blog/')
+
+def blog_update(request, slug):
+    context = {}
+    try:
+        blog_obj = BlogModel.objects.get(slug = slug)
+
+        if blog_obj.user != request.user:
+            return redirect('/')
+
+        initial_dict = {'content': blog_obj.content, 'image': blog_obj.image}
+        form = BlogForm(initial=initial_dict)
+
+        if request.method == 'POST':
+            form = BlogForm(request.POST)
+            blog_obj.image = request.FILES['image']
+            blog_obj.title = request.POST.get('title')
+            blog_obj.user = request.user
+
+            if form.is_valid():
+                blog_obj.content = form.cleaned_data['content']
+            
+            blog_obj.save()
+            return redirect('/')
+
+        context['blog_obj'] = blog_obj
+        context['form'] = form
+    except Exception as e:
+        print(e)
+    return render(request, 'update_blog.html', context)
 
 def register_view(request):
     return render(request, 'register.html')
